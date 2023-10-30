642b50c396854e738cd56db27951188780cd4ed0
jkwon-dev <kwonejk@gmail.com>
2023-10-30 23:36:06 +0800

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by kwon eunji on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
