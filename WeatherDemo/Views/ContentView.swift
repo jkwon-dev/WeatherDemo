be30fd4bb110f19d3360c6dfd75cbac67d780a74
jkwon-dev <kwonejk@gmail.com>
2023-11-01 23:50:44 +0800

    weather app completed

--- WeatherDemo/Views/ContentView.swift
+++ WeatherDemo/Views/ContentView.swift
@@ -9,15 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                 weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
         }
-        .padding()
+        .background(Color(hue: 0.682, saturation: 0.965, brightness: 0.483))
+        .preferredColorScheme(.dark)
     }
 }
 
